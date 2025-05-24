7a87202384268a226592d1df23c653958bbe852f
Gigomeni0 <joaoantonio.goncalves2003@gmail.com>
2025-05-24 16:20:07 -0300

    Adição de permissão de acesso ao banco de dados, configurando passagem livre pelo arquivo SecurityConfig

---
+++ src/main/java/com/movexa/api/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.movexa.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.disable());
+        return http.build();
+    }
+}
